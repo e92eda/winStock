4a7219dd2cc255130c4defc0cf0baf8b617ea805
kunieda <e92da@mac.com>
2022-09-12 01:37:28 +0900

    9/11 str:pkに変更

--- stock/urls.py
+++ stock/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
     path('stock-list/', views.StockListView.as_view(), name="stock_list"),
-    path('stock-detail/<int:pk>/', views.StockDetailView.as_view(), name="stock_detail"),
+    path('stock-detail/<str:pk>/', views.StockDetailView.as_view(), name="stock_detail"),
     # グラフ描画
-    path('stock-detail/<int:pk>/plot/', views.get_svg, name='plot'),
+    path('stock-detail/<str:pk>/plot/', views.get_svg, name='plot'),
     # path('plot/', views.get_svg, name='plot'),
     path('stock-create/', views.StockCreateView.as_view(), name="stock_create"),
-    path('stock-update/<int:pk>/', views.StockUpdateView.as_view(), name="stock_update"),
-    path('stock-delete/<int:pk>/', views.StockDeleteView.as_view(), name="stock_delete"),
+    path('stock-update/<str:pk>/', views.StockUpdateView.as_view(), name="stock_update"),
+    path('stock-delete/<str:pk>/', views.StockDeleteView.as_view(), name="stock_delete"),
 
     path('stock-import/', views.StockImportView.as_view(), name="stock_import"),
     path('stock-export/', views.stock_export, name='stock_export'),
